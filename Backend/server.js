dac7ad669d78a6f1d105819ad48beda0ee29fb71
Anurag Jain <jainanurag941@gmail.com>
2022-12-05 19:27:13 +0530

    Init Commit

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const dbConnect = require("./config/db/dbConnect");
+dotenv.config();
+
+const app = express();
+
+dbConnect();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+});
