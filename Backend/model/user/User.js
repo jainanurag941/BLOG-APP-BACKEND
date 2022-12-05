f837369db56d078bb6e7f68f70ad3e6fcf5f08c2
Anurag Jain <jainanurag941@gmail.com>
2022-12-05 23:15:13 +0530

    Hash User Password Bcryptjs

--- Backend/model/user/User.js
+++ Backend/model/user/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema(
   {
@@ -98,6 +99,13 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+//Hash Password
+userSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
 
 const User = mongoose.model("User", userSchema);
 
