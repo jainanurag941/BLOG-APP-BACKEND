411c60fd1aec8d50d43da21d6090f6a85ac036ce
Anurag Jain <jainanurag941@gmail.com>
2022-12-05 20:13:34 +0530

    User Registration Controller 1

--- Backend/server.js
+++ Backend/server.js
@@ -4,11 +4,26 @@ const dbConnect = require("./config/db/dbConnect");
 dotenv.config();
 
 const app = express();
-
+//DB
 dbConnect();
 
-const PORT = process.env.PORT || 5000;
+//Register
+app.post("/api/users/register", (req, res) => {
+  res.json({ user: "User Registered" });
+});
 
+//Login
+app.post("/api/users/login", (req, res) => {
+  res.json({ user: "User Logged In" });
+});
+
+//Fetch all users
+app.get("/api/users", (req, res) => {
+  res.json({ user: "fetch all users" });
+});
+
+//server
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
 });
